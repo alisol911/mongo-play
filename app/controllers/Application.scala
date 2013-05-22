187cacb4097269823ba5f3f8b0ca15343a904dd3
Ali Soleimani <alisol911@gmail.com>
2013-05-22 11:18:00 +0430

    implmented simple service

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,21 @@ package controllers
 import play.api._
 import play.api.mvc._
 
-object Application extends Controller {
+import play.modules.reactivemongo.MongoController
+import play.modules.reactivemongo.json.collection.JSONCollection
+
+object Application extends Controller with MongoController {
   
+  def collection( collection: String ): JSONCollection = db.collection[ JSONCollection ]( collection )
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def createNote = Action( parse.json ) { implicit request ⇒
+    Async {
+      collection( "note" ).insert( request.body ).map( lastError ⇒ Ok( if ( lastError.inError ) "error " + lastError else lastError.elements.mkString( "," ) ) )
+    }
   }
   
 }
--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,7 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
-  )
-
+    "org.reactivemongo" %% "play2-reactivemongo" % "0.9")
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
