bd451384a7ab6a707c14d0114709d588a3813ac3
Ali Soleimani <alisol911@gmail.com>
2013-05-31 22:39:51 +0430

    added simple dojo client

--- public/main.js
+++
@@ -1,8 +0,0 @@
-function create(){
-    require(["dojo/query", "dojo/on", "dojo/topic", "dojo/request/xhr", "dojo/json",
-            "dojo/NodeList-dom", "dojo/NodeList-manipulate", "dojo/parser", "dojo/ready", "dojo/domReady!"],
-        function (query, on, topic, xhr, json) {
-            xhr("/note", {method: "POST", data: json.stringify({ name: query("#text").val() }), headers: { 'Content-Type': 'application/json' }});
-        }
-    );
-}
---
+++ public/scripts/widgets/Command.js
@@ -0,0 +1,18 @@
+define([
+    "dojo/_base/declare",
+    "dijit/_WidgetBase",
+    "dijit/_TemplatedMixin",
+    "dojo/request/xhr", 
+    "dojo/json",
+    "dojo/text!./templates/Command.htm"
+], function(declare, _WidgetBase, _TemplatedMixin, xhr, json, template) {
+return declare([_WidgetBase, _TemplatedMixin], {
+    templateString: template,
+    _run: function() {
+        xhr("/service/entity/note", {
+            method: "POST",
+            data: json.stringify({ name: this.text.value }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
+});});
