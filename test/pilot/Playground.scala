387f5ff339f434c31b045592fc9b700731867908
Ali Soleimani <alisol911@gmail.com>
2013-06-04 18:16:34 +0430

    fixed bug in cirteria query param

--- test/pilot/Playground.scala
+++ test/pilot/Playground.scala
@@ -9,13 +9,20 @@ import org.specs2.runner.JUnitRunner
 import org.junit.runner.RunWith
 import play.api.libs.json._
 
-@RunWith( classOf[JUnitRunner] )
+@RunWith( classOf[ JUnitRunner ] )
 class Playground extends Specification {
   "find note" in {
     val json = Json.obj( "name" -> "test" )
     println( json )
 
     println( json + ( "n2" -> JsString( "test2" ) ) )
-
   }
+
+  "query" in {
+    running( FakeApplication( additionalPlugins = Seq( "play.modules.reactivemongo.ReactiveMongoPlugin" ) ) ) {
+      val findAll = route( FakeRequest( GET, "/service/entity/note?criteria=%7B%22tags%22%3A%7B%22%24in%22%3A%5B%22test1%22%2C%22test4%22%5D%7D%7D" ) ).get
+      status( findAll ) must equalTo( OK )
+      println( contentAsString( findAll ) )
+    }
+
 }
