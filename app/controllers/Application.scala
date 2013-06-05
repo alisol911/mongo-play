e29420efdcc88eec3c7cd02fec2bec6252237f6b
Ali Soleimani <alisol911@gmail.com>
2013-06-05 16:17:13 +0430

    refactor in controller

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,10 +11,38 @@ import play.api.http.Writeable
 
 object Application extends Controller with MongoController {
 
-  def collection( collectionName: String ): JSONCollection = db.collection[ JSONCollection ]( collectionName )
+  private def collection( collectionName: String ): JSONCollection = db.collection[ JSONCollection ]( collectionName )
 
   private implicit def stringToObjectID( id: String ): play.api.libs.json.JsObject = {
     JsObject( Seq( "_id" -> JsObject( Seq( "$oid" -> JsString( id ) ) ) ) )
+  }
+
+  private def operationWraper( op: () ⇒ scala.concurrent.Future[ reactivemongo.core.commands.LastError ],
+                               result: () ⇒ Result ): Result = {
+    Async {
+      op().map { lastError ⇒
+        if ( lastError.inError )
+          InternalServerError( lastError.toString )
+        else
+          result()
+      } recover {
+        case e ⇒
+          InternalServerError( e.getMessage )
+      }
+    }
+  }
+
+  private def findWraper( op: () ⇒ scala.concurrent.Future[ List[ play.api.libs.json.JsObject ] ],
+                          result: ( List[ JsObject ] ) ⇒ Result ): Result = {
+    Async {
+      op().map{ ( l: List[ JsObject ] ) ⇒
+        if ( l.isEmpty )
+          NoContent
+        else
+          result( l )
+      }
+    }
+
   }
 
   def index = Action {
@@ -22,73 +50,28 @@ object Application extends Controller with MongoController {
   }
 
   def create( entity: String ) = Action( parse.json ) { implicit request ⇒
-    Async {
-      val id = BSONObjectID.generate.stringify
-      val json = id ++ request.body.as[ JsObject ]
-      collection( entity ).insert( json ).map { lastError ⇒
-        if ( lastError.inError )
-          InternalServerError( lastError.toString )
-        else
-          Ok( json )
-      } recover {
-        case e ⇒
-          InternalServerError( e.getMessage )
-      }
-    }
+    val id = BSONObjectID.generate.stringify
+    val json = id ++ request.body.as[ JsObject ]
+    operationWraper( () ⇒ collection( entity ).insert( json ), () ⇒ Ok( json ) )
   }
 
   def find( entity: String, id: String ) = Action { implicit request ⇒
-    Async {
-      collection( entity ).find[ JsObject ]( id )
-        .cursor[ play.api.libs.json.JsObject ]
-        .toList.map{ ( f: List[ JsObject ] ) ⇒
-          if ( f.isEmpty )
-            NoContent
-          else
-            Ok( f.head )
-        }
-    }
+    findWraper( () ⇒ collection( entity ).find[ JsObject ]( id )
+      .cursor[ play.api.libs.json.JsObject ].toList, ( l ) ⇒ if ( l.isEmpty ) NoContent else Ok( l.head ) )
   }
 
   def findAll( entity: String, criteria: String ) = Action { implicit request ⇒
-    Async {
-      collection( entity ).find( Json.parse( criteria ) )
-        .cursor[ play.api.libs.json.JsObject ]
-        .toList.map{ ( f: List[ JsObject ] ) ⇒
-          if ( f.isEmpty )
-            NoContent
-          else
-            Ok( JsArray( f ) )
-        }
-    }
+    findWraper( () ⇒ collection( entity ).find( Json.parse( criteria ) )
+      .cursor[ play.api.libs.json.JsObject ]
+      .toList, ( l ) ⇒ if ( l.isEmpty ) NoContent else Ok( JsArray( l ) ) )
   }
 
   def edit( entity: String, id: String ) = Action( parse.json ) { implicit request ⇒
-    Async {
-      val json = request.body.as[ JsObject ]
-      collection( entity ).update[ JsObject, JsObject ]( id, Json.obj( "$set" -> json ) ).map { lastError ⇒
-        if ( lastError.inError )
-          InternalServerError( lastError.toString )
-        else
-          Ok( json )
-      } recover {
-        case e ⇒
-          InternalServerError( e.getMessage )
-      }
-    }
+    val json = request.body.as[ JsObject ]
+    operationWraper( () ⇒ collection( entity ).update[ JsObject, JsObject ]( id, Json.obj( "$set" -> json ) ), () ⇒ Ok( json ) )
   }
 
   def delete( entity: String, id: String ) = Action {
-    Async {
-      collection( entity ).remove[ JsObject ]( id ).map { lastError ⇒
-        if ( lastError.inError )
-          InternalServerError( lastError.toString )
-        else
-          NoContent
-      } recover {
-        case e ⇒
-          InternalServerError( e.getMessage )
-      }
-    }
+    operationWraper( () ⇒ collection( entity ).remove[ JsObject ]( id ), () ⇒ NoContent )
   }
 }
